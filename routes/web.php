5dcc1e800d8203a3ed028acefcd6f0ee11f5c61d
roth0010 <89872836+roth0010@users.noreply.github.com>
2022-06-01 15:39:57 +0200

    fixed HTTP error, fixed Laravel app name

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect(route('login'));
+    return view('auth.login');
 });
 
 Route::get('beans', function () {
