a13d292dd0dfa21b87b8ada4840c3e35a6e67c6e
roth0010 <89872836+roth0010@users.noreply.github.com>
2022-06-01 15:19:28 +0200

    Oops, I committed to Main.

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,7 +3,7 @@
         <img src="\img\tinygans.png"
              alt="Goose hehe"
              class="brand-image img-circle elevation-3">
-        <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
+        <span class="brand-text font-weight-light">Honk!</span>
     </a>
 
     <div class="sidebar">
