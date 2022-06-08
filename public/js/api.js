aae48eaf5e277360fc1f2e22dba9ceba07903991
roth0010 <89872836+roth0010@users.noreply.github.com>
2022-06-08 09:38:52 +0200

    Added Colour-Changing functionality

--- public/js/api.js
+++ public/js/api.js
@@ -10,24 +10,44 @@ document.getElementById('region').onchange = function() {
             return response.json() })
         .then(data => {
             const alerts = document.getElementById('alerts')
+
             // This clears previous alerts on the div
             alerts.innerHTML = '';
+
             // Function to display a message if there are no alerts to display.
             if(data.features.length === 0) {
-                console.log('this one`s empty boss')
+                console.log('This one`s empty boss!')
                 let warning = document.createElement('strong')
                 warning.innerText = "No Alerts to Display."
                 alerts.append(warning)
             }
+
             // Function that runs through all the alerts and prints them to the dom
             for(let i = 0; i < data.features.length; i++){
                 let alertData = data.features[i];
                 let newAlert;
+
+                // Creates the Div for the card
                 newAlert = document.createElement('div')
-                newAlert.className = 'card bg-gray presentationCard'
-                // create the overarching Div
+
+                // Determines what color to make the card
+                const severityData = alertData.properties.severity;
+                if(severityData === 'Extreme') {
+                    newAlert.className = 'card bg-purple presentationCard'
+                } else if(severityData === "Severe") {
+                    newAlert.className = 'card bg-red presentationCard'
+                } else if(severityData === "Moderate") {
+                    newAlert.className = 'card bg-orange presentationCard'
+                } else if(severityData === 'Minor') {
+                    newAlert.className = 'card bg-yellow presentationCard'
+                } else {
+                    newAlert.className = 'card bg-gray presentationCard'
+                }
+
+                // create the internal Div used for styling
                 let margin = document.createElement('div')
                 margin.className = 'margin-top1'
+
                 // create the headline in bold
                 let strong;
                 strong = document.createElement('strong')
@@ -36,44 +56,53 @@ document.getElementById('region').onchange = function() {
                 title.innerText = alertData.properties.headline
                 strong.appendChild(title)
                 margin.appendChild(strong)
+
                 // create the instruction text
                 if(alertData.properties.instruction !== null){
                     let instruction = document.createElement('p')
                     instruction.innerHTML = '<strong>Instructions: </strong>' + alertData.properties.instruction
                     margin.appendChild(instruction)
                 }
+
                 // create the severity text
                 let severity = document.createElement('p')
-                severity.innerHTML = '<strong>Severity: </strong>' + alertData.properties.severity
+                severity.innerHTML = '<strong>Severity: </strong>' + severityData
                 margin.appendChild(severity)
+
                 // create the certainty text
                 let certainty = document.createElement('p')
                 certainty.innerHTML = '<strong>Certainty: </strong>' + alertData.properties.certainty
                 margin.appendChild(certainty)
+
                 // create the event text
                 let event = document.createElement('p')
                 event.innerHTML = '<strong>Event: </strong>' + alertData.properties.event
                 margin.appendChild(event)
+
                 // create the description text
                 let description = document.createElement('p')
                 description.innerHTML = '<strong>Description: </strong>' + alertData.properties.description
                 margin.appendChild(description)
+
                 // create the area description text
                 let areaDesc = document.createElement('p')
                 areaDesc.innerHTML = '<strong>Affected Areas: </strong>' + alertData.properties.areaDesc
                 margin.appendChild(areaDesc)
+
                 // create the area description text
                 let smallSenderName = document.createElement('small')
                 let senderName = document.createElement('p')
                 senderName.innerHTML = 'Reporting Station: ' + alertData.properties.senderName
                 smallSenderName.appendChild(senderName)
                 margin.appendChild(smallSenderName)
+
                 // create the effective text
                 let smallEffective = document.createElement('small')
                 let effective = document.createElement('p')
                 effective.innerHTML = 'Effective: ' + alertData.properties.effective
                 smallEffective.appendChild(effective)
                 margin.appendChild(smallEffective)
+
                 // create the ends text
                 if(alertData.properties.ends !== null) {
                     let smallEnds = document.createElement('small')
@@ -82,10 +111,10 @@ document.getElementById('region').onchange = function() {
                     smallEnds.appendChild(ends)
                     margin.appendChild(smallEnds)
                 }
+
                 // Final Append Child
                 newAlert.appendChild(margin)
                 alerts.append(newAlert)
-                console.log(alertData)
             }
         })
         .catch(error => {
